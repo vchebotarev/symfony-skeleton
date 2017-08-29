b578c0afada93b078409cae75f6508b8676d00dd
vchebotarev <v.yu.chebotarev@mail.ru>
2017-08-29 23:13:21 +0300

    password validator

---
+++ src/App/Symfony/Validator/Constraints/Password.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Symfony\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraints\Length;
+
+class Password extends Combination
+{
+    /**
+     * @inheritdoc
+     */
+    protected function getConstraints()
+    {
+        $constraints = [
+            new Length([
+                //todo translations
+                'minMessage' => 'Пароль должен содержать не менее 6 символов',
+                'maxMessage' => 'Пароль должен содержать не более 128 символов',
+                'min'        => 6,
+                'max'        => 128,
+            ]),
+        ];
+        return $constraints;
+    }
+
+}
