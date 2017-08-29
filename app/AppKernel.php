b208315b4fd2a8960c2527ade6d507f29fd2efc9
vchebotarev <v.yu.chebotarev@mail.ru>
2017-08-29 16:21:33 +0300

    friendsofsymfony/user-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
 
             new AppBundle\AppBundle(),
         ];
