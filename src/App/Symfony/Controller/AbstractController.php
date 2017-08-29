b37fadc072acccf234bd8bed692f181514402857
vchebotarev <v.yu.chebotarev@mail.ru>
2017-08-29 17:26:30 +0300

    AbstractController

---
+++ src/App/Symfony/Controller/AbstractController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Symfony\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
+
+abstract class AbstractController extends BaseController
+{
+
+}
