26b43496f5b3b7f0668191091ab803ae52ced01b
vchebotarev <v.yu.chebotarev@mail.ru>
2017-08-29 15:58:42 +0300

    index.php

---
+++ web/index.php
@@ -0,0 +1,36 @@
+<?php
+
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpFoundation\Request;
+
+require __DIR__.'/../vendor/autoload.php';
+
+if (!getenv('APP_ENV')) {
+    (new Dotenv())->load(__DIR__.'/../.env');
+}
+
+$env   = getenv('APP_ENV');
+$debug = getenv('APP_DEBUG');
+
+if ($debug) {
+    if (isset($_SERVER['HTTP_CLIENT_IP'])
+        || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+        || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) || PHP_SAPI === 'cli-server')
+    ) {
+        header('HTTP/1.0 403 Forbidden');
+        exit('You are not allowed to access this file.');
+    }
+
+    Debug::enable();
+}
+
+$kernel = new AppKernel($env, $debug);
+//$kernel = new AppCache($kernel);
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+//Request::enableHttpMethodParameterOverride();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
