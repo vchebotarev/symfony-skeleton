2e754a138e63a4fe6c6c1cadae704b685c272a8e
vchebotarev <v.yu.chebotarev@mail.ru>
2017-08-29 18:34:44 +0300

    ReopeningConnectionWrapper

---
+++ src/App/Doctrine/DBAL/ReopeningConnectionWrapper.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Doctrine\DBAL;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DBALException;
+
+/**
+ * https://stackoverflow.com/questions/14258591/the-entitymanager-is-closed
+ */
+class ReopeningConnectionWrapper extends Connection
+{
+    /**
+     * @inheritdoc
+     */
+    public function insert($tableName, array $data, array $types = array())
+    {
+        try {
+            parent::insert($tableName, $data, $types);
+        } catch (DBALException $e) {
+            if (!$this->isConnected()) {
+                $this->connect();
+            }
+            throw new DBALException($e->getMessage());
+        }
+    }
+
+}
