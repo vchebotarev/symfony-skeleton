f01036d44b1cbcc1cb028d32692d980e9a98c17d
vchebotarev <v.yu.chebotarev@mail.ru>
2017-08-30 00:07:42 +0300

    ExceptionController

---
+++ src/AppBundle/Controller/ExceptionController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\TwigBundle\Controller\ExceptionController as BaseExceptionController;
+use Symfony\Component\HttpFoundation\Request;
+
+class ExceptionController extends BaseExceptionController
+{
+    /**
+     * Можно было бы просто подложить вьюшки в app/Resources/Twig/Exception , но мне так нагляднее
+     * @see http://symfony.com/doc/current/controller/error_pages.html
+     * @inheritdoc
+     */
+    protected function findTemplate(Request $request, $format, $code, $showException)
+    {
+        if (!$showException) {
+            $template = sprintf('@App/Exception/error%s.%s.twig', $code, $format);
+            if ($this->templateExists($template)) {
+                return $template;
+            }
+            $template = sprintf('@Twig/Exception/error.%s.twig', $format);
+            if ($this->templateExists($template)) {
+                return $template;
+            }
+        }
+        return parent::findTemplate($request, $format, $code, $showException);
+    }
+
+}
