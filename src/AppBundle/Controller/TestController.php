80c26177e1cc29032a02ff97cedad7da16bfc454
vchebotarev <v.yu.chebotarev@mail.ru>
2017-08-30 14:35:46 +0300

    TestController

---
+++ src/AppBundle/Controller/TestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use App\Symfony\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TestController extends AbstractController
+{
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function testAction(Request $request)
+    {
+
+        return new Response('Test');
+    }
+
+}
