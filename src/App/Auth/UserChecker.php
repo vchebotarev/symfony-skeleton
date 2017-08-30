bd13540958c1f3cbff40216cd3cee2562e656157
vchebotarev <v.yu.chebotarev@mail.ru>
2017-08-30 14:26:33 +0300

    UserChecker

---
+++ src/App/Auth/UserChecker.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Auth;
+
+use Symfony\Component\Security\Core\User\UserChecker as BaseUserChecker;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker extends BaseUserChecker
+{
+    /**
+     * @inheritDoc
+     */
+    public function checkPreAuth(UserInterface $user)
+    {
+        return;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function checkPostAuth(UserInterface $user)
+    {
+        //Необходимо, чтобы выводило пользователю о "проблемах" с аккаунтом, только после того как он ввел правильный пароль, а не каждому встречному
+        parent::checkPreAuth($user);
+        parent::checkPostAuth($user);
+    }
+
+}
