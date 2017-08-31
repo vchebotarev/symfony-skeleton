0fed56f9f772c63d3b6e0187544b21e30f49486d
vchebotarev <v.yu.chebotarev@mail.ru>
2017-08-31 17:06:53 +0300

    kernel.not_debug parameter

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,6 +5,9 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+    /**
+     * @inheritdoc
+     */
     public function registerBundles()
     {
         $bundles = [
@@ -39,23 +42,49 @@ class AppKernel extends Kernel
         return $bundles;
     }
 
+    /**
+     * @inheritDoc
+     */
+    protected function getKernelParameters()
+    {
+        return array_merge(
+            parent::getKernelParameters(),
+            [
+                'kernel.not_debug' => !$this->debug,
+            ]
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
     public function getRootDir()
     {
         return __DIR__;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getCacheDir()
     {
         return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getLogDir()
     {
         return dirname(__DIR__).'/var/logs';
     }
 
+    /**
+     * @inheritdoc
+     */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
+
 }
