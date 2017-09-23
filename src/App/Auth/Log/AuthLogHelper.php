0b2b48393d18c112ad4076afddd91f81bb58b5fb
vchebotarev <v.yu.chebotarev@mail.ru>
2017-09-23 13:11:05 +0300

    AuthLogHelper

---
+++ src/App/Auth/Log/AuthLogHelper.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Auth\Log;
+
+use AppBundle\Entity\UserAuthLog;
+
+class AuthLogHelper
+{
+    /**
+     * @var array
+     */
+    protected $typeNames = [
+        UserAuthLog::TYPE_UNKNOWN           => 'Неизвестно',
+        UserAuthLog::TYPE_USERNAME_PASSWORD => 'Пароль',
+        UserAuthLog::TYPE_REMEMBER_ME       => 'Куки',
+        UserAuthLog::TYPE_OAUTH             => 'OAuth',
+        UserAuthLog::TYPE_LINK              => 'Ссылка',
+    ];
+
+    /**
+     * @param int $type
+     * @return string
+     */
+    public function getTypeName(int $type) : string
+    {
+        if (isset($this->typeNames[$type])) {
+            return $this->typeNames[$type];
+        }
+        return $this->typeNames[UserAuthLog::TYPE_UNKNOWN];
+    }
+
+}
---
+++ src/AppBundle/Twig/Extension/AuthLogExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Twig\Extension;
+
+use App\Auth\Log\AuthLogHelper;
+
+class AuthLogExtension extends \Twig_Extension
+{
+    /**
+     * @var AuthLogHelper
+     */
+    protected $authLogHelper;
+
+    /**
+     * @param AuthLogHelper $authLogHelper
+     */
+    public function __construct(AuthLogHelper $authLogHelper)
+    {
+        $this->authLogHelper = $authLogHelper;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('auth_log_type_name', [$this, 'getName']),
+        ];
+    }
+
+    /**
+     * @param int $type
+     * @return string
+     */
+    public function getName(int $type) : string
+    {
+        return $this->authLogHelper->getTypeName($type);
+    }
+
+}
