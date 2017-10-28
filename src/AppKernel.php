a1f938eeaca252389eba8e09c38b545cf95b1e26
vchebotarev <v.yu.chebotarev@mail.ru>
2017-10-28 13:14:55 +0300

    symfony 4 structure

--- src/AppKernel.php
+++ src/AppKernel.php
@@ -87,7 +87,7 @@ class AppKernel extends Kernel
      */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load($this->getProjectDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
 
 }
