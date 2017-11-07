41c36fd4da97aa4e368bfecaee714c5fc6056104
vchebotarev <v.yu.chebotarev@mail.ru>
2017-11-07 10:51:45 +0300

    authentication handlers ajax

---
+++ src/Symfony/Security/Authentication/AjaxAuthenticationFailureHandler.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Symfony\Security\Authentication;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authentication\DefaultAuthenticationFailureHandler;
+use Symfony\Component\Security\Http\HttpUtils;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class AjaxAuthenticationFailureHandler extends DefaultAuthenticationFailureHandler
+{
+    /**
+     * @var TranslatorInterface
+     */
+    protected $translator;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct(HttpKernelInterface $httpKernel, HttpUtils $httpUtils, array $options = [], LoggerInterface $logger = null, TranslatorInterface $translator)
+    {
+        parent::__construct($httpKernel, $httpUtils, $options, $logger);
+
+        $this->translator = $translator;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        $parentReturn = parent::onAuthenticationFailure($request, $exception);
+        if ($request->isXmlHttpRequest()) {
+            return new JsonResponse([
+                'success' => false,
+                'error'   => $this->translator->trans($exception->getMessageKey(), $exception->getMessageData()),
+                'url'     => $this->options['failure_path'],
+            ]);
+        }
+        return $parentReturn;
+    }
+
+}
---
+++ src/Symfony/Security/Authentication/AjaxAuthenticationSuccessHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Symfony\Security\Authentication;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\DefaultAuthenticationSuccessHandler;
+
+class AjaxAuthenticationSuccessHandler extends DefaultAuthenticationSuccessHandler
+{
+    /**
+     * @inheritDoc
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
+    {
+        if ($request->isXmlHttpRequest()) {
+            return new JsonResponse([
+                'success' => true,
+                'url'     => $this->determineTargetUrl($request),
+            ]);
+        }
+        return parent::onAuthenticationSuccess($request, $token);
+    }
+
+}
