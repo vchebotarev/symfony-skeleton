f58a437b89669ee78d2d6d4ed7cf86b64b0fb196
vchebotarev <v.yu.chebotarev@mail.ru>
2017-11-11 18:23:15 +0300

    mysql ONLY_FULL_GROUP_BY disable

---
+++ src/Doctrine/DBAL/Event/Listeners/MysqlOnlyFullGroupByDisable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Doctrine\DBAL\Event\Listeners;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\DBAL\Driver\AbstractMySQLDriver;
+use Doctrine\DBAL\Event\ConnectionEventArgs;
+use Doctrine\DBAL\Events;
+
+class MysqlOnlyFullGroupByDisable implements EventSubscriber
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getSubscribedEvents()
+    {
+        return [Events::postConnect];
+    }
+
+    /**
+     * @param ConnectionEventArgs $args
+     */
+    public function postConnect(ConnectionEventArgs $args)
+    {
+        /*
+        или это можно сделать глобально создав миграцию с
+        SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+         */
+
+        if (!$args->getConnection()->getDriver() instanceof AbstractMySQLDriver) {
+            return;
+        }
+        $args->getConnection()->executeQuery("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));");
+    }
+
+}
