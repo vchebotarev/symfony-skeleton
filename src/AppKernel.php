963d9f529cce5352e9ec1704a84aba5a33cdfb1e
vchebotarev <v.yu.chebotarev@mail.ru>
2018-07-29 23:49:33 +0300

    nelmio api doc config (not full)

--- src/AppKernel.php
+++ src/AppKernel.php
@@ -36,6 +36,7 @@ class AppKernel extends Kernel
             new \HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
             new \FOS\RestBundle\FOSRestBundle(),
             new \JMS\SerializerBundle\JMSSerializerBundle(),
+            new \Nelmio\ApiDocBundle\NelmioApiDocBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
