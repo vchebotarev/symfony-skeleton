d3064a4c0666e0924da74b41f39d049e70f50d7c
vchebotarev <v.yu.chebotarev@mail.ru>
2019-11-04 14:46:02 +0300

    composer require symfony/orm-pack

--- config/bundles.php
+++ config/bundles.php
@@ -1,6 +1,9 @@
 <?php
 
 return [
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
