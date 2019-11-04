49727a9f3ddc3753d9627528f9cb126448cc1529
vchebotarev <v.yu.chebotarev@mail.ru>
2019-11-04 17:55:58 +0300

    composer require csa/guzzle-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    Csa\Bundle\GuzzleBundle\CsaGuzzleBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
