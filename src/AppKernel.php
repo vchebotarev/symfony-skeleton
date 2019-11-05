56bb22e6c16770d7703a7d36c7d86f5684e8a7b0
vchebotarev <v.yu.chebotarev@mail.ru>
2019-11-05 03:35:42 +0300

    fix project directories

--- src/AppKernel.php
+++ src/AppKernel.php
@@ -51,24 +51,8 @@ class AppKernel extends Kernel
         );
     }
 
-    public function getRootDir()
-    {
-        return __DIR__;
-    }
-
-    public function getCacheDir()
-    {
-        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
-    }
-
-    public function getLogDir()
-    {
-        return dirname(__DIR__).'/var/logs';
-    }
-
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load($this->getProjectDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
-
 }
